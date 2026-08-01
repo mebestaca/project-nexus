947cc3b4bef44baf5a06bb11d8551c6233f1ed66
reinerjustin <reinerjustin.realica@edu.sait.ca>
2026-08-01 16:12:45 -0600

    added paddle component for pingpong

---
+++ src/components/pingpong/Paddle.tsx
@@ -0,0 +1,39 @@
+import { View, StyleSheet } from "react-native";
+
+interface Props {
+    x:number;
+    y:number;
+    width:number;
+    height:number;
+}
+
+export default function Paddle({
+    x,
+    y,
+    width,
+    height,
+}: Props) {
+
+    return (
+        <View
+            style={[
+                styles.paddle,
+                {
+                    left:x,
+                    top:y,
+                    width,
+                    height,
+                }
+            ]}
+        />
+    );
+}
+
+const styles = StyleSheet.create({
+
+    paddle:{
+        position:"absolute",
+        backgroundColor:"white",
+    }
+
+});
