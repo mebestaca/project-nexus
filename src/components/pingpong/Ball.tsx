e2a21410d683fc9a8f946f25405529963311533f
reinerjustin <reinerjustin.realica@edu.sait.ca>
2026-08-01 16:09:09 -0600

    added ball component for pingpong

---
+++ src/components/pingpong/Ball.tsx
@@ -0,0 +1,37 @@
+import { View, StyleSheet } from "react-native";
+
+interface Props {
+    x: number;
+    y: number;
+    size: number;
+}
+
+export default function Ball({
+    x,
+    y,
+    size,
+}: Props) {
+
+    return (
+        <View
+            style={[
+                styles.ball,
+                {
+                    left:x,
+                    top:y,
+                    width:size,
+                    height:size,
+                    borderRadius:size/2,
+                }
+            ]}
+        />
+    );
+}
+
+const styles = StyleSheet.create({
+
+    ball:{
+        position:"absolute",
+        backgroundColor:"white",
+    }
+});
