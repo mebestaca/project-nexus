f7ba4281a436d15a3f4c694e6c2be49989da82e3
Marc Edison Estaca <mebestaca@gmail.com>
2026-08-01 05:24:20 -0600

    firebase setup

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,75 @@
-import { Text, View, StyleSheet } from "react-native";
+import { auth } from "@/firebase/config";
+import { Stack } from "expo-router";
+import { signInAnonymously } from "firebase/auth";
+import { Alert, Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const initialValues = {
+  name: "",
+};
 
 export default function Index() {
+  const handleSubmit = async (
+    values: typeof initialValues,
+    {
+      setSubmitting,
+    }: {
+      setSubmitting: (isSubmitting: boolean) => void;
+    },
+  ) => {
+    try {
+      await signInAnonymously(auth);
+      // router.replace("/employee");
+    } catch (error: any) {
+      let message = "Something went wrong. Please try again.";
+
+      switch (error.code) {
+        case "auth/invalid-email":
+          message =
+            "Please enter a valid email address (e.g., name@example.com).";
+          break;
+
+        case "auth/invalid-credential":
+          message = "Invalid email or password.";
+          break;
+
+        case "auth/user-not-found":
+          message = "No account exists with this email.";
+          break;
+
+        case "auth/wrong-password":
+          message = "Incorrect password. Try again.";
+          break;
+
+        case "auth/network-request-failed":
+          message = "Network error. Check your internet connection.";
+          break;
+
+        default:
+          message = error.message;
+      }
+
+      Alert.alert("Sign In Failed", message);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
-    </View>
+    <>
+      <Stack.Screen options={{ headerShown: false }} />
+      <SafeAreaView>
+        <View>
+          <Text>🐾</Text>
+          <Text>Welcome</Text>
+
+          <Text></Text>
+
+          <Pressable onPress={() => {}}>
+            <Text>Play</Text>
+          </Pressable>
+        </View>
+      </SafeAreaView>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
