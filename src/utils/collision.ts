1a09a951cdaa4247b58087b3abb79fbf3d7f18eb
reinerjustin <reinerjustin.realica@edu.sait.ca>
2026-08-01 17:48:48 -0600

    added collision in utils for pingpong

---
+++ src/utils/collision.ts
@@ -0,0 +1,30 @@
+import {
+    Ball,
+    Paddle,
+  } from "@/types/pingpong";
+  
+  export function paddleCollision(
+    ball: Ball,
+    paddle: Paddle
+  ) {
+  
+    return (
+      ball.x < paddle.x + paddle.width &&
+      ball.x + ball.size > paddle.x &&
+      ball.y < paddle.y + paddle.height &&
+      ball.y + ball.size > paddle.y
+    );
+  
+  }
+  
+  export function wallCollision(
+    ball: Ball,
+    width:number
+  ) {
+  
+    return (
+      ball.x <= 0 ||
+      ball.x >= width - ball.size
+    );
+  
+  }
