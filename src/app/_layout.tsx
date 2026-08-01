c0902925165d06cb7e1319b6dee7d2f1b949870d
Marc Edison Estaca <mebestaca@gmail.com>
2026-08-01 15:16:16 -0600

    added lobby and create routes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,22 @@
+import { AuthProvider, useAuth } from "@/context/AuthContext";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <AuthProvider>
+      <RootLayoutNav />
+    </AuthProvider>
+  );
+}
+
+function RootLayoutNav() {
+  const { loading, user } = useAuth();
+  return (
+    <Stack>
+      <Stack.Protected guard={!!user}>
+        <Stack.Screen name="lobby" options={{ title: "Lobby" }} />
+        <Stack.Screen name="create" options={{ title: "Create" }} />
+      </Stack.Protected>
+    </Stack>
+  );
 }
