b934d643e3d8afd3d8ca6d045b04761a6e312787
reinerjustin <reinerjustin.realica@edu.sait.ca>
2026-08-01 13:36:12 -0600

    updated layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,20 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{ title: "Tic Tac Toe" }}
+      />
+      <Stack.Screen
+        name="lobby"
+        options={{ title: "Waiting Room" }}
+      />
+      <Stack.Screen
+        name="game"
+        options={{ title: "Game" }}
+      />
+    </Stack>
+  );
 }
