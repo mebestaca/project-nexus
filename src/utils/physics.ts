c69f29420696c8402ba73bd6754bd297cf810b72
reinerjustin <reinerjustin.realica@edu.sait.ca>
2026-08-01 17:48:01 -0600

    added physics in utils for pingpong

---
+++ src/utils/physics.ts
@@ -0,0 +1,39 @@
+import { Ball } from "@/types/pingpong";
+
+export function moveBall(ball: Ball): Ball {
+
+  return {
+    ...ball,
+
+    x: ball.x + ball.velocityX,
+
+    y: ball.y + ball.velocityY,
+  };
+
+}
+
+export function bounceHorizontal(
+  ball: Ball
+): Ball {
+
+  return {
+    ...ball,
+
+    velocityX:
+      -ball.velocityX,
+  };
+
+}
+
+export function bounceVertical(
+  ball: Ball
+): Ball {
+
+  return {
+    ...ball,
+
+    velocityY:
+      -ball.velocityY,
+  };
+
+}
