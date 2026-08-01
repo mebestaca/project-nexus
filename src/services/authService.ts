1e6270f9f396367532fdeb9216056ea014477ad1
Marc Edison Estaca <mebestaca@gmail.com>
2026-08-01 15:26:08 -0600

    accepting authservice

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,27 +1,13 @@
-import {
-    signInAnonymously,
-    updateProfile,
-  } from "firebase/auth";
-  
-  import { auth } from "@/firebase/config";
-  
-  
-  export async function signInUser(
-    displayName: string
-  ) {
-  
-    const credential =
-      await signInAnonymously(auth);
-  
-  
-    await updateProfile(
-      credential.user,
-      {
-        displayName,
-      }
-    );
-  
-  
-    return credential.user;
-  
-  }
+import { signInAnonymously, updateProfile } from "firebase/auth";
+
+import { auth } from "@/firebase/config";
+
+export async function signInUser(displayName: string) {
+  const credential = await signInAnonymously(auth);
+
+  await updateProfile(credential.user, {
+    displayName,
+  });
+
+  return credential.user;
+}
