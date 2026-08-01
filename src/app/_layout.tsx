a03d8305dad50231d18fea4af4d08e6a2b4a940c
Marc Edison Estaca <mebestaca@gmail.com>
2026-08-01 15:28:40 -0600

    fixing layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,7 +3,6 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-<<<<<<< HEAD
     <AuthProvider>
       <RootLayoutNav />
     </AuthProvider>
@@ -21,21 +20,3 @@ function RootLayoutNav() {
     </Stack>
   );
 }
-=======
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{ title: "Tic Tac Toe" }}
-      />
-      <Stack.Screen
-        name="lobby"
-        options={{ title: "Waiting Room" }}
-      />
-      <Stack.Screen
-        name="game"
-        options={{ title: "Game" }}
-      />
-    </Stack>
-  );
-}
->>>>>>> 1f148f7fcfaa8ea09fe85cfbf10f1c1a6ba812dd
