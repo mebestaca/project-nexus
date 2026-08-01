878692d6714d87659925a9d0d9d8e7e31c14d1a0
Marc Edison Estaca <mebestaca@gmail.com>
2026-08-01 15:18:05 -0600

    added lobby schema. will most likely get removed later

---
+++ src/app/types/lobby.ts
@@ -0,0 +1,21 @@
+export type GameType = "tictactoe" | "arkanoid" | "bullethell";
+
+export interface Lobby {
+  id: string;
+  lobbyName: string;
+  gameType: GameType;
+  host: string;
+  players: number;
+  maxPlayers: number;
+  status: "waiting" | "playing" | "finished";
+}
+
+export interface CreateLobbyValues {
+  lobbyName: string;
+  gameType: GameType;
+  maxPlayers: number;
+}
+
+export default function _() {
+  return null;
+}
