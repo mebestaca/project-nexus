b8f03df5021f536396303ae8d88387960fa7e45a
Marc Edison Estaca <mebestaca@gmail.com>
2026-08-01 23:21:47 -0600

    modifying interface for synced tracking

--- src/types/pingpong.ts
+++ src/types/pingpong.ts
@@ -1,26 +1,28 @@
 export interface Ball {
-    x: number;
-    y: number;
-    size: number;
-    velocityX: number;
-    velocityY: number;
+  x: number;
+  y: number;
+  size: number;
+  velocityX: number;
+  velocityY: number;
 }
 
 export interface Paddle {
-    x: number;
-    y: number;
-    width: number;
-    height: number;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
 }
 
 export interface Score {
-    player1: number;
-    player2: number;
+  player1: number;
+  player2: number;
 }
 
-export interface PingPongState {
-    ball: Ball;
-    player1Paddle: Paddle;
-    player2Paddle: Paddle;
-    score: Score;
+export interface PongGameState {
+  ball: Ball;
+  player1Paddle: Paddle;
+  player2Paddle: Paddle;
+  score: Score;
+  player1Id: string;
+  player2Id: string;
 }
