c2768a11f57181d896a865dc60b0e48c7094b9d3
Marc Edison Estaca <mebestaca@gmail.com>
2026-08-03 18:00:19 -0600

    removed header

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -29,7 +29,10 @@ function RootLayoutNav() {
       <Stack.Screen name="index" options={{ headerShown: false }} />
 
       <Stack.Protected guard={!!user}>
-        <Stack.Screen name="lobby" options={{ title: "Lobby" }} />
+        <Stack.Screen
+          name="lobby"
+          options={{ title: "Lobby", headerShown: false }}
+        />
         <Stack.Screen name="create" options={{ title: "Create" }} />
         <Stack.Screen
           name="rps"
@@ -39,7 +42,10 @@ function RootLayoutNav() {
           name="tictactoe"
           options={{ title: "TicTacToe", headerShown: false }}
         />
-        <Stack.Screen name="waiting" options={{ title: "Waiting" }} />
+        <Stack.Screen
+          name="waiting"
+          options={{ title: "Waiting", headerShown: false }}
+        />
         <Stack.Screen
           name="fourinarow"
           options={{ title: "ConnectFour", headerShown: false }}
--- src/components/auth/SigninForm.tsx
+++ src/components/auth/SigninForm.tsx
@@ -1,4 +1,3 @@
-import { Stack } from "expo-router";
 import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import DisplayNameField from "./DisplayName";
@@ -9,8 +8,6 @@ import { styles } from "@/styles/shared";
 export default function SigninForm() {
   return (
     <>
-      <Stack.Screen options={{ headerShown: false }} />
-
       <SafeAreaView>
         <View style={styles.container}>
           <DisplayNameField />
